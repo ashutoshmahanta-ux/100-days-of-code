f75b5003e027fe780f9e179455ae0d5888a61f13
ashutoshmahanta-ux <ashutoshhh.021@gmail.com>
2025-09-28 23:29:08 +0530

    Add Q7 and Q8 solutions for Day 4

---
+++ DAY4/Q7.c
@@ -0,0 +1,39 @@
+/*Q7: Write a program to swap two numbers without using a third variable.
+
+/*
+Sample Test Cases:
+Input 1:
+10 20
+Output 1:
+After swap: 20 10
+
+Input 2:
+7 14
+Output 2:
+After swap: 14 7
+
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a,b;
+    
+    printf("enter first number:");
+    scanf("%d", &a);
+    
+    printf("enter second number:");
+    scanf("%d", &b);
+    
+    printf("the number before swap: %d %d", a,b);
+    
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    
+    printf("the number after swap: %d %d", a,b);
+    return 0;
+
+}
